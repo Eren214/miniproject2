637d207a6c2d0a4133c6d2be15149d95603388ad
mark estrella <mark.estrella214@gmail.com>
2022-11-03 18:58:27 +0800

    initial layout

---
+++ src/Footer.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class Footer extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+    return(
+        <section className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3  ">
+                    <h2>Useful Links</h2>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Use</p>
+                    <p>Return Policy</p>
+                    <p>Discount Coupons</p>
+                    </div>
+                    <div className="col-md-3 ">
+                    <h2>Our Company</h2>
+                    <p>About Us</p>
+                    <p>Partners</p>
+                    <p>Careers</p>
+                    <p>Affiliate Programs</p>
+                    </div>
+                    <div className="col-md-3 ">
+                    <h2>Follow Us On</h2>
+                    <p>Facebook</p>
+                    <p>Instagram</p>
+                    <p>Youtube</p>
+                    <p>LinkedIn</p>
+                    </div>
+                    <div className="col-md-3 footer-image">
+                    <h2>Download App</h2>
+                    <img id="playstore" className="img-fluid" src="./goggleapp.png"/>
+                    <img id="applestore" className="img-fluid" src="./apolstore.png"/>
+                    </div>
+                </div>
+                <hr/>
+                <p className="copyright text-center">&#169;TicketLab, All rights reserved 2022</p>
+            </div>
+        </section>   
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {FaAngleRight } from "react-icons/fa";
+import { FaSistrix } from "react-icons/fa";
+import "./App.css";
+function Navbar(){
+
+    const [showLinks, setShowLinks]=useState(false);
+    return(
+    <div className='Navbar'>
+        <div className='leftSide'>
+            <img id="logo" src="ticket.png"/><h2>ticketlab</h2>
+            <button onClick={()=>setShowLinks(!showLinks)}>Menu<FaAngleRight id="arrow"/></button>
+            <div className='searchbar'>
+                <button id="search"><FaSistrix/></button>
+                <input type="text" placeholder='     Event,artist or location' />
+            </div>   
+        </div>
+        <div className='rightSide'>
+            <div className='links' id={showLinks ? "hidden" : ""}>
+                <a href='/home'>My Tickets</a>
+                <a href='/feedback'>Sign In</a>   
+            </div>
+        </div>
+    </div>
+    )
+}
+export default Navbar;
